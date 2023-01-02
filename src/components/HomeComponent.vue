d995bd877fcc7a7df6b8483c9ef2a5aea14c072c
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-02 12:09:01 -0700

    Starting to get the all restaurants to display onto the page

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,42 +1,90 @@
 <template>
-    <div>
-        <section class="home" ref="home" v-scroll-reveal>
-            <div class="home__container bd-container bd-grid">
-                <div class="home__data">
-                    <h1 class="home__title">Foodiee</h1>
-                    <h2 class="home__subtitle">Try the best food of <br> the week.</h2>
-                    <router-link to="" class="button" color="#FFF">View Menu</router-link>
-                </div>
-
-                <img src="@/assets/home.png" alt="" class="home__img">
-            </div>
-        </section>
+  <div>
+    <div class="alert">
+      <v-alert
+        v-if="alert"
+        outlined
+        type="warning"
+        prominent
+        border="left"
+        dismissible
+        transition="fade-transition"
+      >
+        {{ message }}
+      </v-alert>
     </div>
+    <section class="home" ref="home" v-scroll-reveal>
+      <div class="home__container bd-container bd-grid">
+        <div class="home__data">
+          <h1 class="home__title">Foodiee</h1>
+          <h2 class="home__subtitle">
+            Try the best food of <br />
+            the week.
+          </h2>
+          <router-link to="" class="button" color="#FFF">View Menu</router-link>
+        </div>
+
+        <img src="@/assets/home.png" alt="" class="home__img" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import axios from "axios";
+
+export default {
+  mounted() {
+    axios
+      .request({
+        url: `http://127.0.0.1:5000/api/all-restaurants`,
+      })
+      .then((response) => {
+        this.restaurants = response["data"];
+      })
+      .catch((error) => {
+        this.message = "Sorry, an error has occurred. Please, reload the page."
+        this.alert = true
+        error;
+      });
+  },
+
+  data() {
+    return {
+      restaurants: [],
+      message: "",
+      alert: false
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "@/scss/variables.scss";
 
 .home__container {
-    height: calc(100vh - $--header-height);
-    align-content: center;
+  height: calc(100vh - $--header-height);
+  align-content: center;
 }
 
 .home__title {
-    font-size: $--biggest-font-size;
-    color: $--first-color;
-    margin-bottom: $--mb-1;
+  font-size: $--biggest-font-size;
+  color: $--first-color;
+  margin-bottom: $--mb-1;
 }
 
 .home__subtitle {
-    font-size: $--h1-font-size;
-    color: $--title-color;
-    margin-bottom: $--mb-4;
+  font-size: $--h1-font-size;
+  color: $--title-color;
+  margin-bottom: $--mb-4;
+}
+
+.alert {
+  display: grid;
+  place-items: center;
+
+  > v-alert {
+    max-width: 50%;
+  }
 }
 </style>
