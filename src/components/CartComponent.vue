e9977993c4c82005a0ee110918de8918190de174
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-06 17:42:44 -0700

    Organizing the item onto the page

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
         <section class="cart section bd-container" ref="cart" v-scroll-reveal>
-            <div class="cart__container bd-grid">
+            <div class="cart__container bd-grid" v-for="(item, index) in items" :key="index">
                 <div class="cart__data">
-                    <span class="section-subtitle">About us</span>
-                    <h2 class="section-title">We cook the best <br> tasty food.</h2>
-                    <p class="cart__description">We cook the best food in the entire city, with excellent     
-                    customer service,
-                    the best meals and at the best price, visit us.</p>
-                    <a href="#" class="button">Explore History</a>
+                    <span class="section-subtitle">{{item['name']}}</span>
+                    <h2 class="section-title">{{item['price']}}</h2>
+                    <p class="cart__description">{{item['description']}}</p>
+                    <button href="#" class="button">Delete item</button>
                 </div>
 
-                <img src="@/assets/about.jpg" alt="" class="cart__img">
+                <img :src="item['file_name']" alt="" class="cart__img">
+            </div>
+        </section>
+        <section>
+            <div>
+                <p>Total: R$ {{}}</p>
+                <button>Confirm</button>
             </div>
         </section>
     </div>
@@ -21,8 +25,7 @@
 import cookies from "vue-cookies"
     export default {
         mounted () {
-            let items = cookies.get('cart')
-            this.items = items
+            this.items = cookies.get('cart')
         },
 
         data() {
@@ -35,6 +38,10 @@ import cookies from "vue-cookies"
 
 <style lang="scss" scoped>
 @import '@/scss/variables.scss';
+
+.cart__container {
+    margin-bottom: 64px;
+}
 
 .cart__data {
     text-align: center;
