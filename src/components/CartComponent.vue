5024f6162e0e7bdb4cc2ab0ef2030c458b335e01
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-07 12:55:46 -0700

    Adding more style and a confirm order button

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -15,6 +15,14 @@
         </div>
       </div>
     </section>
+
+    <div class="confirm__container">
+        <div class="confirm__line"></div>
+        <div class="confirm-div">
+            <p>Total $: {{}}</p>
+            <button class="button" @click="confirm_order(restaurant, $event)">Confirm Order</button>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -100,5 +108,30 @@ export default {
   font-size: 1.5rem;
   cursor: pointer;
   background-color: #e53935;
+}
+
+.confirm__container {
+    display: grid;
+    place-items: center;
+    margin-top: 48px;
+
+    >.confirm__line {
+        width: 80%;
+        height: .02rem;
+        background-color: $--text-color-light;
+    }
+
+    >.confirm-div {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        place-items: center;
+        justify-content: center;
+        gap: 24px;
+        margin-top: 24px;
+
+        >p {
+            justify-content: center;
+        }
+    }
 }
 </style>
