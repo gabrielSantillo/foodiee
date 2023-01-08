73ec875a72d1a2503ac0917d7c2bd27bca4e9a6b
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-07 18:16:20 -0700

    Changing how the confirm_order send the axios request

--- src/components/ConfirmOrderComponent.vue
+++ src/components/ConfirmOrderComponent.vue
@@ -14,17 +14,51 @@
 
 <script>
 import cookies from "vue-cookies";
+import axios from "axios"
 export default {
 
     methods: {
         confirm_order() {
-            
+            let menu_items_id = [];
+      /* for loop that goes through to the foods and add the food id in the menu itens array */
+      for (let i = 0; i < this.items.length; i++) {
+        menu_items_id.push(this.items[i][`menu_item_id`]);
+      }
+      /* axios request to the client-order API */
+      axios
+        .request({
+          url: `http://127.0.0.1:5000/api/client-order`,
+          headers: {
+            token: `${cookies.get(`client_token`)}`,
+          },
+          method: `POST`,
+          data: {
+            menu_items: menu_items_id,
+            restaurant_id: cookies.get(`restaurant_id`),
+          },
+        })
+        .then((response) => {
+          /* on success grab the order_id response */
+          this.order_id.push(response[`data`][`order_id`]);
+          /* stringfy the order id */
+          let order_id_json = JSON.stringify(this.orders_id);
+          /* set the cookie with the JSON value of the order id */
+          cookies.set(`order_id`, order_id_json);
+
+          alert("order made")
+        })
+        .catch((error) => {
+          error;
+          /* on failure show a message */
+          alert(`Sorry, an error have occured`);
+        });
         }
     },
     data() {
         return {
             total_order: 0,
-            items: undefined
+            items: undefined,
+            order_id: []
         }
     },
 
