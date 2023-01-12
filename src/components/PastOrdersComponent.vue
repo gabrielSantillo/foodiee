eb4c3ef7f33782df688eebbe3d77f39621392519
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-12 16:34:19 -0700

    Starting to add style to the items of the past orders

--- src/components/PastOrdersComponent.vue
+++ src/components/PastOrdersComponent.vue
@@ -1,50 +1,105 @@
 <template>
-  <div>
-    <section class="past__orders__container section bd-container">
-        <div class="order__line"></div>
+  <div v-scroll-reveal>
+    <section class="section bd-container">
+      <div class="order__line"></div>
       <span class="section-subtitle">Past orders</span>
       <h2 class="section-title">Wanna remake an order?</h2>
-    </section>
 
-    <section v-for="(order, index) in past_orders" :key="index" >
-      <div>
-        <div>
+      <div class="menu__container bd-grid">
+        <div class="menu__content" v-for="(order, index) in past_orders" :key="index">
+
           <div v-for="(item, index) in order['menu_items']" :key="index">
-              <p>{{item['name']}}</p>
-              <p>${{item['price']}}</p>
+              <img :src="item['file_name']" alt="" class="menu__img" />
+              <h3 class="menu__name">{{ item["name"] }}</h3>
+              <span class="menu__price">${{ item["price"] }}</span>
           </div>
-          <img src="" alt="">
-        </div>
-        <div>
-          <p>Total ${{order['total_order']}}</p>
-          <button>Again</button>
+
+          <div>
+            <span class="menu__price">Total ${{ order["total_order"] }}</span>
+            <i
+            @click="make_order(order, $event)"
+            class="button menu__button bx bx-cart-alt"></i>
+          </div>
+
         </div>
       </div>
     </section>
+
+
   </div>
 </template>
 
 <script>
-import cookies from "vue-cookies"
+import cookies from "vue-cookies";
 export default {
-    data() {
-        return {
-            past_orders: undefined
-        }
-    },
-    mounted () {
-        this.past_orders = cookies.get('past_orders')
-        this.past_orders
-    },
+  data() {
+    return {
+      past_orders: undefined,
+    };
+  },
+  mounted() {
+    this.past_orders = cookies.get("past_orders");
+    this.past_orders;
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/variables.scss";
+
+.menu__container {
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  column-gap: 48px;
+  justify-content: center;
+}
+
+.menu__content {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background-color: $--container-color;
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 4px rgba(3, 74, 40, 0.15);
+  padding: 1.5rem;
+  margin: 0 16px;
+}
+
+.menu__img {
+  width: 200px;
+  align-self: center;
+  margin-bottom: $--mb-2;
+}
+
+.menu__name,
+.menu__price {
+  font-weight: $--font-semi-bold;
+  color: $--title-color;
+}
+
+.menu__name {
+  font-size: $--normal-font-size;
+}
+
+.menu__detail {
+  margin-bottom: $--mb-1;
+}
+
+.menu__button {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  padding: 0.9375rem 1.2195rem;
+  border-radius: 0.5rem 0 0.5rem 0;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+
 .order__line {
-    width: 100%;
-    height: 0.02rem;
-    background-color: $--text-color-light;
-    margin: 24px 0;
-    justify-self: center;
-  }
+  width: 100%;
+  height: 0.02rem;
+  background-color: $--text-color-light;
+  margin: 24px 0;
+  justify-self: center;
+}
 </style>
