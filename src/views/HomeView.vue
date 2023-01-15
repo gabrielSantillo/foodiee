19f5c0a2d27caf3dfefdcb09d53eb47befe1e06a
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-15 16:06:27 -0700

    Adding a token validation  for the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,25 @@
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import HomeComponent from '@/components/HomeComponent.vue'
 import FooterComponent from '@/components/FooterComponent.vue'
+import cookies from "vue-cookies"
+import axios from "axios"
 export default {
+  mounted () {
+    axios
+        .request({
+          url: `http://127.0.0.1:5000/api/client-token`,
+          headers: {
+            token: `${cookies.get(`client_token`)}`,
+          },
+        })
+        .then((response) => {
+          response
+        })
+        .catch((error) => {
+          this.$router.push(`/`);
+          error;
+        });
+  },
   components: { HeaderComponent, HomeComponent, FooterComponent },
 }
 </script>
